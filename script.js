db7e8902bfb349e9909f4bd6d8768ea18691841c
Carolina-Zanardi <c.zanardi@uni9.edu.br>
2026-05-20 22:59:54 -0300

    adicionar projeto BurguerHouse

---
+++ script.js
@@ -0,0 +1,25 @@
+const lanches = document.querySelectorAll('.lanche');
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add('show');
+
+        } else {
+
+            entry.target.classList.remove('show');
+
+        }
+
+    });
+
+}, {
+    threshold: 0.75
+});
+
+lanches.forEach((lanche) => {
+    observer.observe(lanche);
+});
